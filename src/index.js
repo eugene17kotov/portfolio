4f483902ea1be946c027476c423636864faf8ed0
Yevhenii Kobets <eu.kobets@gmail.com>
2023-06-08 15:07:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// root.render;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter basename="/">
